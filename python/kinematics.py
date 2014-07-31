425dc6eb09f1bee69e2408ead7f9372b81a3f91f
Shivesh Kumar <skumar@kitasan.laas.fr>
2014-07-31 15:13:32 +0200

    Body, Joint, Model, Kinematics exposed.

---
+++ python/kinematics.py
@@ -0,0 +1,110 @@
+import sys
+sys.path.append('/home/skumar/src/rbdlpy/build')
+sys.path.append('/home/skumar/src/eigenpy/build')
+
+import numpy as np
+import libmodel as md
+import libjoint as jt
+import libbody as bd
+import libkinematics as km
+import libeigenpy as eigenpy
+
+# This example has been inspired from example named "simple" in rbdl.
+
+m = md.Model()
+
+# Set the gravity vector
+m.gravity = np.array([0., -9.81, 0.])
+
+# Defining the first body
+mass = 1.0
+com = np.array([0.5, 0., 0.])
+radiusofgyration = np.array([1., 1., 1.])
+body1 = bd.Body(mass, com, radiusofgyration)
+# Defining the first joint
+jointaxis1 = np.array([0., 1., 0.])
+joint1 = jt.Joint(jt.JointType.JointTypeRevolute, jointaxis1)
+# Defining the transfomation between base body(rootbody) and body1
+rot = np.matrix([[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]])
+trans = np.array([0., 0., 0.])
+# Exposing the constructor of SpatialTransform class 
+st = bd.SpatialTransform(rot, trans)
+
+body1_id = m.AddBody(0, st, joint1, body1, "Link1")
+print 'Body with Id', body1_id, 'added to the root body.'
+
+# Defining the second body
+mass = 1.0
+com = np.array([0., 0.5, 0.])
+radiusofgyration = np.array([1., 1., 1.])
+body2 = bd.Body(mass, com, radiusofgyration)
+# Defining the first joint
+jointaxis2 = np.array([0., 0., 1.])
+joint2 = jt.Joint(jt.JointType.JointTypeRevolute, jointaxis2)
+# Defining the transfomation between body1 and body2
+rot = np.matrix([[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]])
+trans = np.array([1., 0., 0.])
+# Exposing the constructor of SpatialTransform class 
+st = bd.SpatialTransform(rot, trans)
+
+body2_id = m.AddBody(body1_id, st, joint2, body2, "Link2")
+print 'Body with Id', body2_id, 'added to the body with Id', body1_id
+
+# Defining the third body
+mass = 0.
+com = np.array([0.5, 0., 0.])
+radiusofgyration = np.array([1., 1., 1.])
+body3 = bd.Body(mass, com, radiusofgyration)
+# Defining the first joint
+jointaxis3 = np.array([0., 0., 1.])
+joint3 = jt.Joint(jt.JointType.JointTypeRevolute, jointaxis3)
+# Defining the transfomation between body1 and body2
+rot = np.matrix([[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]])
+trans = np.array([0., 1., 0.])
+# Exposing the constructor of SpatialTransform class 
+st = bd.SpatialTransform(rot, trans)
+
+body3_id = m.AddBody(body1_id, st, joint3, body3, "Link3")
+print 'Body with Id', body3_id, 'added to the body with Id', body2_id
+
+dof = m.dof_count	# DoF of the model
+Q = np.zeros(dof)	# Zero vector for joint position
+QDot = np.zeros(dof)	# Zero vector for joint velocity
+QDDot = np.zeros(dof)	# Zero vector for joint acceleration
+
+km.UpdateKinematics(m, Q, QDot, QDDot)
+#km.UpdateKinematicsCustom(m, Q, QDot, QDDot)	# Does not work, not so important to expose anyways!!
+print km.CalcBodyToBaseCoordinates(m, Q, 1, np.array([0.5, 0., 0.]), True)
+print km.CalcBaseToBodyCoordinates(m, Q, 1, np.array([0.5, 0., 0.]), True)
+print km.CalcBodyWorldOrientation (m, Q, 1, True)
+
+# Wrapper function for CalcPointJacobian. It returns the matrix G instead of taking it as an reference argument. Please note that the function signature has been modified. However, function name remains the same. Check RBDL documentation.
+print km.CalcPointJacobian(m, Q, 1, np.array([0.5, 0., 0.]), True)
+
+point_position = np.array([0.5, 0., 0.])
+print km.CalcPointVelocity(m, Q, QDot, 1, point_position, True) 
+print km.CalcPointAcceleration(m, Q, QDot, QDDot, 1, point_position, True)
+
+Qinit = Q
+
+body_id = md.pyvec_uint()	# defining a std::vector<unsigned int>
+body_id.append(3)	# Add a body_id in the vector for which we want to solve the inverse kinematics(we can add more for a tree type system)
+
+body_point = md.pyvec_vec3d()	# defining a std::vector<Math::Vector3d>
+v3d = np.array([0., 1., 0.])	# Add a body_point(or current end effector pos) in local body coordinates in the vector for solving the inverse kinematics(we can add more for a tree type system)
+body_point.append(v3d)
+
+target_pos = md.pyvec_vec3d()	# defining a std::vector<Math::Vector3d>
+v3d = np.array([1., 0., 0.])	# Add a body_point(or target end effector pos) in local body coordinates in the vector for solving the inverse kinematics(we can add more for a tree type system)
+target_pos.append(v3d)
+
+Qres = np.zeros(dof)	# Output joint position vector
+
+step_tol = 1.0e-12
+lamda = 0.01
+max_iter = 50
+# Wrapper function for InverseKinematics. It returns the matrix Qres instead of taking it as an reference argument. Please note that the function signature has been modified. However, function name remains the same. Check RBDL documentation.
+Qres = km.InverseKinematics(m, Qinit, body_id, body_point, target_pos, step_tol, lamda, max_iter) 	 # Does not work! Qres is the output joint positions. How to make Python understand that?
+print Qres
+
+#print km.foo(2,5)
---
+++ unittest/kinematics.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2014, Shivesh KUMAR, LAAS-CNRS
+ *
+ * This file is part of rbdlpy.
+ * eigenpy is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * eigenpy is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.  You should
+ * have received a copy of the GNU Lesser General Public License along
+ * with eigenpy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define EIGEN_DONT_VECTORIZE
+#define EIGEN_DISABLE_UNALIGNED_ARRAY_ASSERT 
+
+#include "rbdlpy/rbdlpy.hpp"
+#include <boost/python.hpp>
+
+using namespace boost::python;
+using namespace std;
+using namespace eigenpy;
+
+// Thin wrapper function for CalcPointJacobian. It returns the matrix G. Please note that the function signature has been modified. However, function name remains the same.
+RigidBodyDynamics::Math::MatrixNd CalcPointJacobian(RigidBodyDynamics::Model model, const RigidBodyDynamics::Math::VectorNd Q, unsigned int body_id, const RigidBodyDynamics::Math::Vector3d point_position, bool update_kinematics=true)
+{
+	RigidBodyDynamics::Math::MatrixNd G = RigidBodyDynamics::Math::MatrixNd::Zero (model.dof_count, model.dof_count);
+	RigidBodyDynamics::CalcPointJacobian (model, Q, body_id, point_position, G, update_kinematics=true);
+	return G;
+}
+
+// Thin wrapper function for InverseKinematics.
+RigidBodyDynamics::Math::VectorNd InverseKinematics(RigidBodyDynamics::Model model, const RigidBodyDynamics::Math::VectorNd Qinit, const std::vector< unsigned int > body_id, const std::vector< RigidBodyDynamics::Math::Vector3d > body_point, const std::vector< RigidBodyDynamics::Math::Vector3d > target_pos, double step_tol=1.0e-12, double lambda=0.01, unsigned int max_iter=50)
+{
+	bool soln_found;
+	RigidBodyDynamics::Math::VectorNd Qres = RigidBodyDynamics::Math::VectorNd::Zero (model.dof_count);
+	soln_found = RigidBodyDynamics::InverseKinematics (model, Qinit, body_id, body_point, target_pos, Qres, step_tol=1.0e-12, lambda=0.01, max_iter=50);
+	if(soln_found)
+	{
+		cout<<"Solution found"<<endl;
+		return Qres;
+	}
+	else
+	{
+		cout<<"IK Solution not found"<<endl;
+		Qres = RigidBodyDynamics::Math::VectorNd::Zero (0); ;		
+		return Qres;
+	}
+}
+
+/*
+void test(int &a, int &b, int &c)
+{
+	c = a + b;
+}
+
+int foo(int a, int b)
+{
+int c;
+test(a,b,c);
+return c;
+}
+*/
+
+BOOST_PYTHON_MODULE(libkinematics)
+{
+	eigenpy::enableEigenPy();
+
+	eigenpy::enableEigenPySpecific<RigidBodyDynamics::Math::Vector3d, Eigen::Vector3d>();
+	eigenpy::enableEigenPySpecific<RigidBodyDynamics::Math::Matrix3d, Eigen::Matrix3d>();
+	eigenpy::enableEigenPySpecific<RigidBodyDynamics::Math::SpatialMatrix, Eigen::Matrix<double, 6, 6> >();
+	eigenpy::enableEigenPySpecific<RigidBodyDynamics::Math::VectorNd, Eigen::VectorXd>();
+
+	class_<RigidBodyDynamics::Math::SpatialTransform>("SpatialTransform", init<>())  
+	.def(init<RigidBodyDynamics::Math::Matrix3d, RigidBodyDynamics::Math::Vector3d>());	// exposing the constructor for storing a spatial transform built by rotation matrix and translation vector
+
+	//Exposing the functions of Kinematics module.
+	def("UpdateKinematics", &RigidBodyDynamics::UpdateKinematics, args("model", "Q", "Qdot", "QDDot"),"Updates and computes velocities and accelerations of the bodies. ");
+	def("UpdateKinematicsCustom", &RigidBodyDynamics::UpdateKinematicsCustom, args("model", "Q", "Qdot", "QDDot"),"Selectively updates model internal states of body positions, velocities and/or accelerations. ");
+	def("CalcBodyToBaseCoordinates", &RigidBodyDynamics::CalcBodyToBaseCoordinates, args("model", "Q", "body_id", "body_point_position", "update_kinematics"),"Returns the base coordinates of a point given in body coordinates.");
+	def("CalcBaseToBodyCoordinates", &RigidBodyDynamics::CalcBaseToBodyCoordinates, args("model", "Q", "body_id", "base_point_position", "update_kinematics"),"Returns the body coordinates of a point given in base coordinates.");
+	def("CalcBodyWorldOrientation", &RigidBodyDynamics::CalcBodyWorldOrientation, args("model", "Q", "body_id", "update_kinematics"),"Returns the orientation of a given body as 3x3 matrix.");
+	def("CalcPointJacobian", CalcPointJacobian, args("model", "Q", "body_id", "point_position", "update_kinematics"),"Computes the point jacobian for a point on a body.");
+	def("CalcPointVelocity", &RigidBodyDynamics::CalcPointVelocity, args("model", "Q", "QDot", "body_id", "point_position", "update_kinematics"),"Computes the velocity of a point on a body.");
+	def("CalcPointAcceleration", &RigidBodyDynamics::CalcPointAcceleration, args("model", "Q", "QDot", "QDDot", "body_id", "point_position", "update_kinematics"),"Computes the acceleration of a point on a body.");
+	def("InverseKinematics", InverseKinematics, args("model", "Qinit", "body_id_vector", "body_point_vector", "target_pos_vector", "step_tolerance", "lambda", "max_iter"),"Computes the inverse kinematics iteratively using a damped Levenberg-Marquardt method (also known as Damped Least Squares method)");
+}
+
