a01420f4ed3be8e6b8f3a463f60374a4765b6011
Shivesh Kumar <skumar@kitasan.laas.fr>
2014-08-05 12:02:06 +0200

    Body, Joint, Model, Kinematics, Dynamics exposed.

--- python/FDTest.py
+++ python/FDTest.py
@@ -8,6 +8,7 @@ import libjoint as jt
 import libbody as bd
 import libdynamics as dm
 import libeigenpy as eigenpy
+import csv
 
 # This example has been inspired from example named "pendulum" in rbdl.
 
@@ -33,11 +34,52 @@ st = bd.SpatialTransform(rot, trans)
 body1_id = m.AddBody(0, st, joint1, body1, "Link1")
 print 'Body with Id', body1_id, 'added to the root body.'
 
+# Defining the second body
+mass = 1.0
+com = np.array([0.5, 0., 0.])
+radiusofgyration = np.array([1., 1., 1.])
+body2 = bd.Body(mass, com, radiusofgyration)
+# Defining the first joint
+jointaxis2 = np.array([0., 0., 1.])
+joint2 = jt.Joint(jt.JointType.JointTypeRevolute, jointaxis2)
+# Defining the transfomation between body1 and body2
+rot = np.matrix([[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]])
+trans = np.array([1., 0., 0.])
+# Exposing the constructor of SpatialTransform class 
+st = bd.SpatialTransform(rot, trans)
+
+body2_id = m.AddBody(body1_id, st, joint2, body2, "Link2")
+print 'Body with Id', body2_id, 'added to the body with Id', body1_id
+
 dof = m.dof_count
 Q = np.zeros(dof)	# Zero vector for joint position
 QDot = np.zeros(dof)	# Zero vector for joint velocity
 QDDot = np.zeros(dof)	# Zero vector for joint acceleration
 Tau = np.zeros(dof)	# Zero vector for joint Torques
-# Wrapper function for ForwardDynamics. It returns the matrix QDDot instead of taking it as an reference argument. Please note that the function signature has been modified. However, function name remains the same. Check RBDL documentation.
-QDDot = dm.ForwardDynamics(m, Q, QDot, Tau)
-print QDDot
+
+# std::vector of spatial_vector type. Contains external forces acting on the system, if any.
+fext = np.array([0., 0., 0., 0., 0., 0.])
+f_ext = dm.pyvec_sv()
+f_ext.append(fext)
+
+# Euler Integrator
+dt = 0.1;		# Time step 0.1 s
+t = 0.0;
+Ts = 10.0;		# Total simulation time(Ts)
+
+f = open('/home/skumar/src/rbdlpy/python/test.csv', 'w')
+fw = csv.writer(f, delimiter=',')
+
+while t<=Ts:
+	QDDot = dm.ForwardDynamics(m, Q, QDot, Tau, f_ext).transpose()
+	print t, 'QDDot', QDDot
+	QDot = QDot + QDDot * dt
+	#print 'QDot', QDot
+	Q = Q + QDot * dt
+	#print t, ",", 'Q', Q
+	t = t + dt
+	q = Q.transpose()
+	#if q[0]<=100 and q[1]<=100:  
+	data = [t, q[0], q[1]]
+	fw.writerow(data)
+f.close()
---
+++ python/plot.py
@@ -0,0 +1,18 @@
+# Reads the output.csv file for simple pendulum and plots the q vs time plot
+import numpy as np
+import matplotlib.pyplot as plt
+#data = np.genfromtxt('/home/skumar/src/rbdl/examples/double-pendulum/build/output.csv', delimiter=',', skip_header=0, skip_footer=0, names=['t', 'q1', 'q2'])
+
+data = np.genfromtxt('/home/skumar/src/rbdlpy/python/test.csv', delimiter=',', skip_header=0, skip_footer=0, names=['t', 'q1', 'q2'])
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+
+ax1.set_title("Time evolution of Joint trajectory")    
+ax1.set_xlabel('Time(s)')
+ax1.set_ylabel('Joint position(rad)')
+
+ax1.plot(data['t'], data['q1'], color='r', label='Joint trajectory')
+#ax1.plot(data['t'], data['q2'], color='b', label='Joint trajectory')
+plt.show()
